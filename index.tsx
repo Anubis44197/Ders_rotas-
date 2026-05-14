6c121adc86f36c8be7c103d099eea752b1a7077b
mehmet arif <90062415+Anubis44197@users.noreply.github.com>
2026-05-14 18:28:19 +0300

    Wrap app root with error boundary

--- index.tsx
+++ index.tsx
@@ -12,7 +12,7 @@ if (!rootElement) {
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
